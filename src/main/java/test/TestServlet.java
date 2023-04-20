e9da9f7783db9260927552d4553edaf2883b38e3
Albert <super870520@gmail.com>
2023-04-20 11:52:51 +0800

    TestServlet.java little change

--- src/main/java/test/TestServlet.java
+++ src/main/java/test/TestServlet.java
@@ -11,6 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Just a testing!");
+		response.getWriter().append("Just a testing!!");
 	}
 }
